b1524346330e55a72624d0a01543d9216911d291
AtlasWongy <atlasyjyj@gmail.com>
2023-10-31 09:36:31 +0800

    Project Init + Linked List
    Linked List Implementation

---
+++ app/src/LinkedList/LinkedList.cs
@@ -0,0 +1,69 @@
+using System;
+
+class LinkedList {
+   public Node head;
+
+   public LinkedList(Node head)
+   {
+        this.head = head;
+   }
+
+   public void AddNode(int data) 
+   { 
+        Node newNode = new Node(data);
+        Node currentNode = head;
+        while(currentNode.next != null)
+        {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+   }
+
+   public void DeleteNode(int data)
+   {
+        if (this.head.data.Equals(data))
+        {
+            head = head.next;
+            return;
+        }
+
+        Node currentNode = head;
+        Node previousNode = null;
+
+        while (currentNode != null)
+        {
+            if (currentNode.data.Equals(data))
+            {
+                previousNode.next = currentNode.next;
+                return;
+            }
+
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+   }
+
+   public int CountNodes(Node head)
+   {
+        int count = 1;
+        Node current = head;
+
+        while (current.next != null)
+        {
+            count++;
+            current = current.next;
+        }
+        return count;
+   }
+
+   public void PrintNodes()
+   {
+        Node currentNode = head;
+        while(currentNode != null)
+        {
+            Console.Write(currentNode.data + " -> ");
+            currentNode = currentNode.next;
+        }
+        Console.WriteLine("null");
+   }
+}
---
+++ app/src/LinkedList/Node.cs
@@ -0,0 +1,7 @@
+class Node {
+    public int data;
+    public Node? next;
+    public Node (int data) {
+        this.data = data;
+    }
+}
